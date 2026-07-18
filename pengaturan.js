acc2f67ff1795b94fedefb9c3031245cbdccfb3c
moodifypreset <moodifypreset@gmail.com>
2026-07-18 12:07:43 +0700

    Add files via upload

---
+++ pengaturan.js
@@ -0,0 +1,10 @@
+// PENGATURAN APLIKASI CLIENT PORTAL[cite: 1]
+const CONFIG_APP = {
+    namaVendor: "Portal Client",[cite: 1]
+    googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo",[cite: 1]
+    whatsappAdmin: "6282117102831",[cite: 1]
+
+    // Konfigurasi Supabase
+    supabaseUrl: "https://ihqafpnqmlhewwyatiau.supabase.co/rest/v1/",
+    supabaseKey: "sb_publishable_ClY-tvZnSpQqrpG2I9a2fA_q6cRhAdC"
+};
